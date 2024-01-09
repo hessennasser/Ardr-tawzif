4b33a120ac20b56d7d01244b387f5a8ef22e9990
hessennasser <hessenaloosh@gmail.com>
2024-01-09 21:42:56 +0200

    link form with server

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Toggle Menu
 const menuIcon = document.querySelector(".toggle-menu");
 const nav = document.querySelector(".nav-bar");
+const apiURL = "https://ardr-tawzif-server.vercel.app";
 
 menuIcon.addEventListener("click", () => {
   if (menuIcon.classList.contains("fa-bars")) {
@@ -15,11 +16,48 @@ menuIcon.addEventListener("click", () => {
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
 
+  // get all jobs
+  const jobsSelect = document.getElementById('jobsSelect');
+  // Fetch all jobs from the server using Axios
+  axios.get(`${apiURL}/all-jobs`)
+    .then(response => {
+      const jobs = response.data.data;
+      // Add job IDs as options to the select element
+      jobs.forEach(item => {
+        const option = document.createElement('option');
+        option.value = item;
+        option.text = item;
+        jobsSelect.appendChild(option);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching jobs:', error);
+    });
+
+  // get sections
+  const sectionsSelect = document.getElementById('sectionsSelect');
+  // Fetch all sections from the server using Axios
+  axios.get(`${apiURL}/all-sections`)
+    .then(response => {
+      const sections = response.data.data;
+      // Add job IDs as options to the select element
+      sections.forEach(item => {
+        const option = document.createElement('option');
+        option.value = item;
+        option.text = item;
+        sectionsSelect.appendChild(option);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching sections:', error);
+    });
+
+  // submit form
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     if (validateForm()) {
-      form.submit();
+      submitFormData();
     }
   });
 
@@ -68,11 +106,12 @@ document.addEventListener("DOMContentLoaded", function () {
       showError("من فضلك، قم بإدخال رقم هاتفك.");
       isValid = false;
       return false;
-    } else if (!/^\d+$/.test(phoneNumber)) {
-      showError("يجب أن يحتوي رقم الهاتف على أرقام فقط.");
-      isValid = false;
-      return false;
-    }
+    } 
+    // else if (!/^\d+$/.test(phoneNumber)) {
+    //   showError("يجب أن يحتوي رقم الهاتف على أرقام فقط.");
+    //   isValid = false;
+    //   return false;
+    // }
 
     if (email.trim() === "") {
       showError("من فضلك، قم بإدخال بريدك الإلكتروني.");
@@ -87,30 +126,43 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function submitFormData() {
-    const formData = new FormData(form);
-    const endpoint = "https://api.web3forms.com/submit";
-    const accessKey = "c0625a52-0cf9-4207-857e-1ec4ff6f9398";
+    const form = document.querySelector("form");
+    const formDataObject = {
+      name: form.querySelector("[name='name']").value,
+      'birth-date': form.querySelector("[name='birth-date']").value,
+      city: form.querySelector("[name='city']").value,
+      nationality: form.querySelector("[name='nationality']").value,
+      gender: form.querySelector("[name='gender']").value,
+      'phone number': form.querySelector("[name='phone number']").value,
+      mail: form.querySelector("[name='mail']").value,
+      section: form.querySelector("[name='sectionsSelect']").value,
+      job: form.querySelector("[name='jobsSelect']").value,
+      experience: form.querySelector("[name='experience']").value,
+      skills: form.querySelector("[name='skills']").value,
+      'english-level': form.querySelector("[name='english-level']").value,
+      'education-level': form.querySelector("[name='education-level']").value,
+      'work under pressure': form.querySelector("[name='work under pressure']").value,
+      'about yourself': form.querySelector("[name='about yourself']").value,
+      'cv link': form.querySelector("[name='cv link']").value,
+    };
 
-    fetch(endpoint, {
-      method: "POST",
-      body: {...formData, access_key: accessKey, subject: "New Submission from ARDR GLOBAL"},
-      headers: {
-        ContentType: "application/json"
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
+    // Use Axios instead of Fetch
+    axios.post(`${apiURL}/submit`, formDataObject)
+      .then((response) => {
+        const data = response.data;
         if (!data.success) {
           console.log(data);
           throw new Error(`HTTP error! Error: ${data.message}`);
         }
         Swal.fire({
           icon: 'success',
-          title: 'Success',
+          title: 'نجح ارسال طلبك',
           text: 'تم إرسال النموذج الخاص بك بنجاح!',
         });
+        form.reset();
+        // window.location.href = `${location.origin}/success.html`;
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error:", error);
         Swal.fire({
           icon: 'error',
@@ -120,7 +172,6 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 
-
   function showError(message) {
     Swal.fire({
       icon: 'error',
@@ -128,6 +179,7 @@ document.addEventListener("DOMContentLoaded", function () {
       text: message,
     });
   }
+
   function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
